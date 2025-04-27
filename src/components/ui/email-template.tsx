f0b013958777fac694107c6c0f1818ee798ba933
Lubasi Milupi <lubasimilupi@gmail.com>
2025-04-27 23:39:42 +0200

    Email functionality try one

---
+++ src/components/ui/email-template.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Template = () => {
+  return (
+    <div>
+      <h1>You have successfully subscribed to STYLAS newsletter!</h1>
+      <p>Kind Regards,</p>
+      <p>Lubasi Milupi</p>
+      <p>MLPLUB001</p>
+    </div>
+  );
+};
+
+export default Template;
