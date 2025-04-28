6c06687c8e6848fed0a314d399463aa962a83623
Lubasi Milupi <lubasimilupi@gmail.com>
2025-04-29 01:46:26 +0200

    Rapboy on a footballers wage

--- src/app/details/[id]/page.tsx
+++ src/app/details/[id]/page.tsx
@@ -1,13 +1,18 @@
 import React from "react";
 import Details from "@/features/details/components/Details";
 
-const ProductDetails = ({ params }: { params: { id: string } }) => {
-  const uuid = params.id;
+const ProductDetails = async ({
+  params,
+}: {
+  params: Promise<{ id: string }>;
+}) => {
+  const { id } = await params;
+  //const uuid = params.id;
   //console.log(uuid);
 
   return (
     <>
-      <Details uuid={uuid} />
+      <Details uuid={id} />
     </>
   );
 };
